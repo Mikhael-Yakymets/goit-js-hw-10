eb57a51a0822a82c5408a267693381ccb1a2856b
Mykhailo Yakymets <vskitano@gmail.com>
2025-03-10 17:56:10 +0200

    Homework 10 done

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -10,9 +10,7 @@ const refs = {
 };
 
 refs.button.addEventListener('click', onClick);
-
 refs.button.disabled = true;
-
 let userSelectedDate = null;
 
 const options = {
@@ -22,7 +20,11 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] < new Date()) {
-      return alert('Please choose a date in the future');
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topCenter',
+      });
     }
     refs.button.disabled = false;
     userSelectedDate = selectedDates[0];
@@ -31,17 +33,34 @@ const options = {
 
 flatpickr(refs.input, options);
 
-const interfaceInput = {
-  enableTime: true,
-  dateFormat: 'Y-m-d H:i',
-};
-
 function onClick() {
   refs.button.disabled = true;
   refs.input.disabled = true;
 
   const timerId = setInterval(() => {
     const diff = userSelectedDate - Date.now();
+
+    //#region Stop Timer
+    if (diff <= 0) {
+      clearInterval(timerId);
+
+      document.querySelector('[data-days]').textContent = '00';
+      document.querySelector('[data-hours]').textContent = '00';
+      document.querySelector('[data-minutes]').textContent = '00';
+      document.querySelector('[data-seconds]').textContent = '00';
+
+      iziToast.success({
+        title: 'Timer Finished',
+        message: 'Time is running out! We congratulate you!',
+        position: 'topCenter',
+      });
+
+      refs.input.disabled = false;
+      refs.button.disabled = false;
+      return;
+    }
+    //#endregion Stop Timer
+
     const { days, hours, minutes, seconds } = convertMs(diff);
 
     document.querySelector('[data-days]').textContent = addLeadingZero(days);
@@ -54,15 +73,12 @@ function onClick() {
 }
 
 //#region Leading Zero
-
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
-
 //#endregion Leading Zero
 
 //#region Convertation Time
-
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -81,5 +97,4 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
 //#endregion Convertation Time
